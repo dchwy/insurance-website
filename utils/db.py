d3b856d66a65af1f50e71fda8ca5cd37b218e7ef
Do Cong Huy <huydocong1502@gmail.com>
2025-05-15 21:34:22 +0700

    updated

--- utils/db.py
+++ utils/db.py
@@ -1,19 +1,10 @@
-import mysql.connector
 import streamlit as st
+import mysql.connector
 
 def get_connection():
-    """
-    Create and return a MySQL database connection using Streamlit secrets.
-    Make sure you have a `.streamlit/secrets.toml` file like this:
-
-    [mysql]
-    host = "localhost"
-    user = "root"
-    password = "your_password"
-    database = "insurance_db"
-    """
     return mysql.connector.connect(
         host=st.secrets["mysql"]["host"],
+        port=st.secrets["mysql"]["port"],
         user=st.secrets["mysql"]["user"],
         password=st.secrets["mysql"]["password"],
         database=st.secrets["mysql"]["database"]
